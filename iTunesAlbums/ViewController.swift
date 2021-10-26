245f7a3ca891a898ae5a686565657786e7bb638c
synthpuh <amaeth.olga@gmail.com>
2021-10-26 14:12:52 +0300

    Initial Commit

---
+++ iTunesAlbums/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesAlbums
+//
+//  Created by Ольга Шубина on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
